bb129ed2b710c04ea695e56a5cd157d402f6783d
Jaden Hope <Jaden_Hope@Yahoo.com>
2021-06-07 11:41:50 +0200

    switched display to template

--- matching-days.js
+++ matching-days.js
@@ -2,20 +2,28 @@ const dateInput1 = document.getElementById("dateInput1");
 const dateInput2 = document.getElementById("dateInput2");
 var date1;
 var date2;
-var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const arrDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",]
+
+document.body.onload = () => {
+    var templateSource = document.querySelector(".template").innerHTML;
+    var compTemplate = Handlebars.compile(templateSource);
+    var weekDisplayElem = document.querySelector(".weekDisplay");
+    var daysOfWeek = [{day : "Sunday"}, {day : "Monday"}, {day : "Tuesday"}, {day : "Wednesday"}, {day : "Thursday"}, {day : "Friday"}, {day : "Saturday"}];
+    var weekDisplayHTML = compTemplate({weekDays : daysOfWeek});
+    weekDisplayElem.innerHTML = weekDisplayHTML;
+}
 
 dateInput1.onchange = function () {
     if (document.querySelector(".red")) {
         document.querySelector(".red").classList.remove("red");}
 
     date1 = new Date(dateInput1.value);
-    console.log(date1);
-    var day = daysOfWeek[date1.getDay()];
+    var day = arrDays[date1.getDay()];
     var selectedDay = document.getElementById(day);
 
     if (document.querySelector(".green")) {
         document.querySelector(".green").classList.remove("green");
-        document.getElementById(daysOfWeek[date2.getDay()]).classList.add("blue");}
+        document.getElementById(arrDays[date2.getDay()]).classList.add("blue");}
 
     if (selectedDay.classList.contains("blue")) {
         selectedDay.classList.remove("blue");
@@ -29,12 +37,12 @@ dateInput2.onchange = function () {
         document.querySelector(".blue").classList.remove("blue");}
 
     date2 = new Date(dateInput2.value);
-    var day = daysOfWeek[date2.getDay()];
+    var day = arrDays[date2.getDay()];
     var selectedDay = document.getElementById(day);
 
     if (document.querySelector(".green")) {
         document.querySelector(".green").classList.remove("green");
-        document.getElementById(daysOfWeek[date1.getDay()]).classList.add("red");}
+        document.getElementById(arrDays[date1.getDay()]).classList.add("red");}
 
     if (selectedDay.classList.contains("red")) {
         selectedDay.classList.remove("red");
